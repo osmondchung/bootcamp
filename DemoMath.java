4f08f917afa881bbae9dc32229c0f570a7e8d5f9
osmondchung <osmondocchung@gmail.com>
2025-08-04 18:09:51 +0800

    update

---
+++ DemoMath.java
@@ -0,0 +1,9 @@
+public class DemoMath {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 9;
+        
+        System.out.println(x+y);
+
+    }
+}
---
+++ Helloworld.java
@@ -0,0 +1,24 @@
+public class Helloworld{
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+
+    // Data Type (int)
+    int x = 1;
+    int y = 2;
+      // Variable cannot be redefine
+    double z = 1.1;
+    
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(z);
+    int a = 10;
+    a = 17;
+    System.out.println(a);
+
+    int b;
+    b = 11;
+    System.out.println(b);
+
+  }
+}
+
